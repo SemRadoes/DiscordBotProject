df4755e58b6a2ae08f5b1e57e7fc3c59c7b3f991
Aws3m <semradijs@hotmail.com>
2021-12-04 23:59:44 +0100

    changes made: DONE: - added more possible message to reply to TO DO: - add the right replies to the messages - add more functionalities

--- src/bot.js
+++ src/bot.js
@@ -6,11 +6,33 @@ const intents = new discord.Intents("32767");
 
 const client = new discord.Client({ intents });
 
+const ps = require("prompt-sync");
+const prompt = ps();
+
 client.on("ready", () => console.log("i am ready master"));
 
 client.on("messageCreate", message => {
-    // console.log(message.content);
-    if(message.content.toLowerCase() === "hello") message.reply("Hi there, what can i do for you?");
+    messageList = ["!Hello", "!How are you?", "!How old are you?"];
+
+    for (value of messageList){
+        switch(value.content.toLowerCase()){
+            case `${value.substring(1)}`:
+                return message.reply("your message should start with '!' ");
+            case `${value}`:
+                return message.reply("Hi there, what can i do for you?");
+            default:
+                return;
+        }
+    }
+    // if(message.content.toLowerCase() === `hello`){
+    //     return WrongMessageReply;
+    // } else if (message.content.toLowerCase() === `!hello`){
+    //     message.reply("Hi there, what can i do for you?");
+    // } else {
+    //     return;
+    // }
+    // const args = message.content.substring(config.prefix.length).split(/ +/);
+    
 });
 
 
