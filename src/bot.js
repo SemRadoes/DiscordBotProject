d944cbf1e0d31dfaf7f2e7114b0058841c38753b
Aws3m <semradijs@hotmail.com>
2021-12-06 21:09:02 +0100

    changes 6/12/21:
    DONE:
    
    - tried other things
    
    TO DO:
    
    - fix say and other commands!
    - Add other functionalities

--- src/bot.js
+++ src/bot.js
@@ -2,39 +2,41 @@ const discord = require("discord.js");
 
 const config = require("./data/config.json");
 
+const fs = require('fs');
+
 const intents = new discord.Intents("32767");
 
 const client = new discord.Client({ intents });
 
-const ps = require("prompt-sync");
-const prompt = ps();
-
 client.on("ready", () => console.log("i am ready master"));
 
+// let today = new Date();
+// let date = `${today.getDay()} ${today.getMonth()} ${today.getFullYear()}`;
+// const file = "C:\Users\semra\Documents\Projects to learn\First Discord Bot\DiscordBotProject\src";
+// const dateCreated = fs.statSync(file).birthtime;
 client.on("messageCreate", message => {
-    messageList = ["!Hello", "!How are you?", "!How old are you?"];
 
-    for (value of messageList){
-        switch(value.content.toLowerCase()){
-            case `${value.substring(1)}`:
-                return message.reply("your message should start with '!' ");
-            case `${value}`:
-                return message.reply("Hi there, what can i do for you?");
-            default:
-                return;
-        }
+    // no prefix!
+    if(!message.content.startsWith(config.prefix)) return;
+
+    // advanced questions!
+    const repeatMe = message.content.split(" ");
+    switch(message.content.toLowerCase()){
+        case "say":
+            message.reply(repeatMe.splice(1));
+            break;
+        case "hello":
+            message.reply("Hi there, what can i do for you?");
+            break;
+        case "how are you?":
+            message.reply("I'm very good, thank you. and you?");
+            break;
+        // case `!When were you created?`:
+        //     return message.reply(`I am created ${dateCreated}`);
+        default:
+            return;
     }
-    // if(message.content.toLowerCase() === `hello`){
-    //     return WrongMessageReply;
-    // } else if (message.content.toLowerCase() === `!hello`){
-    //     message.reply("Hi there, what can i do for you?");
-    // } else {
-    //     return;
-    // }
-    // const args = message.content.substring(config.prefix.length).split(/ +/);
-    
 });
-
 
 client.login(config.token);
 
